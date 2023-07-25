3969ff9893e4edfbc944620d1968bf5b06ca884e
shresht <shresht.b@metamation.com>
2023-07-25 17:06:13 +0530

    changes

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,9 @@
-﻿namespace Training {
+﻿namespace Assignment_1 {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+         Console.WriteLine ("Hello World!");
+         }
       }
+
    }
 }
