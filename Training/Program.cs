5bad232d6e8e9126ee6ea44877bf3ca521b43613
shresht <shresht.b@metamation.com>
2023-07-25 17:12:47 +0530

    Final changes made to A02 assignment

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,23 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, !");
-      }
+         int n, number, guesses;
+         bool game = true;
+         while (game) {
+            number = 0;
+            n = new Random ().Next (1, 100 + 1);
+            guesses = 0;
+            while (number != n) {
+               Console.Write ("Enter a number from 1 to 100: ");
+               number = int.Parse (Console.ReadLine ());
+               if (number > n)
+                  Console.WriteLine ("Your guess is too high,try again");
+               else if (number < n)
+                  Console.WriteLine ("Your guess is too low,try again");
+               guesses++;
+            }
+            Console.WriteLine ("Well done, your guess is correct!");
+            break;
+         }
    }
 }
