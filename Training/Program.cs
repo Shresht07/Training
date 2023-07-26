11302232efd0835a00d213974333598f5164ddf7
shresht <shresht.b@metamation.com>
2023-07-26 09:14:58 +0530

    Final changes done, and pushed to online

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,9 +1,25 @@
 ﻿namespace Assignment_1 {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello World!");
+         int n, number, guesses;
+         bool game = true;
+         while (game) {
+            number = 0;
+            n = new Random ().Next (1, 100 + 1);
+            guesses = 0;
+            while (number != n) {
+               Console.Write ("Enter a number from 1 to 100: ");
+               number = int.Parse (Console.ReadLine ());
+               if (number > n)
+                  Console.WriteLine ("Your guess is too high,try again");
+               else if (number < n)
+                  Console.WriteLine ("Your guess is too low,try again");
+               guesses++;
+            }
+            Console.WriteLine ("Well done, your guess is correct!");
+            break;
          }
+      }
       }
 
    }
-}
