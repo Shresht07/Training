8225b1c9d2af90c7d3bb0b7aa543cf43ef460bf8
Shresh2 <138746515+Shresht07@users.noreply.github.com>
2023-07-26 15:46:47 +0530

    Original script

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,29 +1,7 @@
-﻿int n, number, guesses;
-bool Game=true;
-string play_again;
-while (Game) {
-   number = 0;
-   n = new Random ().Next (1, 100 + 1);
-   guesses = 0;
-   while (number != n) {
-      Console.Write ("Enter a number from 1 to 100: ");
-      number = int.Parse (Console.ReadLine ());
-      if (number > n)
-         Console.WriteLine ("Your guess is too high,try again");
-      else if (number < n)
-         Console.WriteLine ("Your guess is too low,try again");
-      guesses++;
-      if (guesses > 6) {
-         Console.WriteLine ("Sorry, game over :( "); break;
-      }
-   }
-   if (number == n)
-      Console.WriteLine ("Well done, your guess is correct!");
-   Console.WriteLine ("Would You like to play again ? (Yes/No)");
-   play_again = Console.ReadLine ();
-   if (play_again == "Yes" || play_again == "yes")
-      Game = true;
-   else {
-      Console.WriteLine ("Thank you for playing the game !!"); break;
-   }
-}  
+namespace Training {
+    internal class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Hello, World!");
+        }
+    }
+}
