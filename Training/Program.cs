be04720a9b3d98b6e38ccb731ca2c64d265b3b12
Shresh2 <138746515+Shresht07@users.noreply.github.com>
2023-08-01 12:34:46 +0530

    Another version of A02 assignment done and pushed to repo

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,19 @@
-namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
-}
+int LowLimit = 0, HighLimit = 100, nGuesses = 0, NewLimit = (LowLimit + HighLimit) / 2;
+do {
+   Console.WriteLine ($"Find the guessed number :) \n Is the number greater than {NewLimit}: Y/N");
+   switch (Console.ReadKey ().Key) {
+      case ConsoleKey.Y:
+         LowLimit = NewLimit;
+         break;
+      case ConsoleKey.N:
+         HighLimit = NewLimit;
+         break;
+      default:
+         Console.WriteLine ("Enter valid key :(");
+         break;
+   }
+   nGuesses++;
+   NewLimit = (LowLimit + HighLimit) / 2;
+   Console.WriteLine ();
+} while (nGuesses < 6);
+Console.WriteLine ($"{NewLimit + 1} is the guessed number ;)");
