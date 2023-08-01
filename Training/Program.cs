5b9ad78af1281451f07fbe14098b0eaf8a632e6b
Shresh2 <138746515+Shresht07@users.noreply.github.com>
2023-08-01 15:26:31 +0530

    Added comments and changed appropriate variable names

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,19 +1,24 @@
-int LowLimit = 0, HighLimit = 100, nGuesses = 0, NewLimit = (LowLimit + HighLimit) / 2;
+//A Number guessing game code, where the user guesss a number and the computer finds the secret number
+int lowLimit = 0, highLimit = 100, nGuesses = 0, newLimit = (lowLimit + highLimit) / 2;
+Console.WriteLine ("Finding the guessed number :)");
 do {
-   Console.WriteLine ($"Find the guessed number :) \n Is the number greater than {NewLimit}: Y/N");
-   switch (Console.ReadKey ().Key) {
+   //prompts the user to enter y or n wheather the guessed number is lesser than or not
+   Console.WriteLine ($"Is the number lesser than {newLimit}: [Y/N]");
+   switch (Console.ReadKey (true).Key) {
       case ConsoleKey.Y:
-         LowLimit = NewLimit;
+         highLimit = newLimit;
          break;
       case ConsoleKey.N:
-         HighLimit = NewLimit;
+         lowLimit = newLimit;
          break;
       default:
          Console.WriteLine ("Enter valid key :(");
          break;
    }
+   // increments each and every tries made by the user
    nGuesses++;
-   NewLimit = (LowLimit + HighLimit) / 2;
+   newLimit = (lowLimit + highLimit) / 2;
    Console.WriteLine ();
 } while (nGuesses < 6);
-Console.WriteLine ($"{NewLimit + 1} is the guessed number ;)");
+//No.of guesses made by the computer, which must be 6 for numbers in range 1 to 100
+Console.WriteLine ($"{newLimit} is the guessed number ;)"); // prints the guessed number
