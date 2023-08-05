4ef8265d7391678d62d6f37d8a524ecfb98cda96
Shresh2 <138746515+Shresht07@users.noreply.github.com>
2023-08-05 13:35:49 +0530

    Changes done and pushed to repo

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,16 +1,18 @@
 //A Number guessing game code, where the user guesss a number and the computer finds the secret number
-int lowLimit = 1, highLimit = 128, power = 1, remainder = 0, divisor = 2, nGuesses = 0;
-Console.WriteLine ($"Find the guessed number :). Think of a number from {lowLimit} to {highLimit}:");
-do {                                                                                                          
-   Console.WriteLine ($"Is the remainder {remainder}, when you divide the number by {divisor}: [Y/N]");      //prompts the user to enter y or n wheather the remainder is 0 or 1 if divisible by 2
-   switch (Console.ReadKey (true).Key) {
+int power = 1, remainder = 0, divisor = 2, nGuesses = 0, pov = 0;
+Console.WriteLine ("Find the guessed number :) \nThink of a number from 1 to 128...");
+do {
+   //prompts the user to enter y or n wheather the remainder is 0 or 1 if divisible by 2
+   Console.Write ($"\nIs remainder {remainder} or {remainder + Math.Pow (2, pov)} , when you divide the number by {divisor}: [Y for {remainder}/N for {remainder + Math.Pow (2, pov)}] --> ");
+   switch (Console.ReadKey ().Key) {
       case ConsoleKey.Y:
          power++;                                                                                           //power is incremented for each and every valid reply 
-         nGuesses++;                                                                                        //increments each and every tries made by the user
+         nGuesses++;
+         pov++;                                                                                              //increments each and every tries made by the user
          break;
       case ConsoleKey.N:
          remainder = remainder + (divisor / 2);
-         power++; nGuesses++;
+         power++; nGuesses++; pov++;
          break;
       default:
          Console.WriteLine ("Enter valid key :(");
@@ -18,4 +20,4 @@ do {
    }
    divisor = (int)(Math.Pow (2, power));
 } while (nGuesses < 7);                                                                                    //No.of guesses made by the computer, which must be 7 for numbers in range 1 to 128
-Console.WriteLine ($"{remainder} is the guessed number ;)");                                               //prints the guessed number
+Console.WriteLine ($"\n{remainder} is the guessed number ;)");                                             //prints the guessed number
