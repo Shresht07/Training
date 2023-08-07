7850e0c0a7d666a3fe6f96870cc143b10a29bfda
Shresh2 <138746515+Shresht07@users.noreply.github.com>
2023-08-07 10:32:36 +0530

    Default case changed and pushed to repo

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,22 +1,20 @@
 //A Number guessing game code, where the user guesss a number and the computer finds the secret number
-int power = 1, remainder = 0, divisor = 2, nGuesses = 0, pov = 0;
+int power = 1, remainder = 0, divisor = 2, nGuesses = 0;
 Console.WriteLine ("Find the guessed number :) \nThink of a number from 1 to 128...");
 do {
    //prompts the user to enter y or n wheather the remainder is 0 or 1 if divisible by 2
    //Press Y or y for Yes and N or n for No
    Console.Write ($"\nIs remainder {remainder}, when you divide the number by {divisor}: [Y/N] --> ");
-   switch (Console.ReadKey ().Key) {
+   switch (Console.ReadKey (true).Key) {
       case ConsoleKey.Y:
+         Console.WriteLine ('Y');
          power++;                                                                                           //power is incremented for each and every valid reply 
-         nGuesses++;
-         pov++;                                                                                              //increments each and every tries made by the user
-         break;
-      case ConsoleKey.N:
-         remainder = remainder + (divisor / 2);
-         power++; nGuesses++; pov++;
+         nGuesses++;                                                                                        //increments each and every tries made by the user
          break;
       default:
-         Console.WriteLine ("Enter valid key :(");
+         Console.WriteLine ('N');
+         remainder = remainder + (divisor / 2);
+         power++; nGuesses++;
          break;
    }
    divisor = (int)(Math.Pow (2, power));
