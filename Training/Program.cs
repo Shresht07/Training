c91026adb7904e7f973e71e5163b0a9d88907705
Shresh2 <138746515+Shresht07@users.noreply.github.com>
2023-08-08 10:15:26 +0530

    Assignment T01 done and pushed to repo

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,39 @@
-namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+// Number conversion game --> Which converts a decimal number to binary equivalent or Hexadecimal equivalent number based on the user input 
+Console.Write ("Enter a decimal number: ");                                 //Accepts input from user as a decimal number
+var dec = int.Parse (Console.ReadLine ());
+Console.Write ("Binary or Hexadecimal? (B/H): ");                           //prompts the user to enter keys (B or b) fo0r binary and (H or h) for hexadecimal connversion 
+switch (Console.ReadKey (true).Key) {
+   case ConsoleKey.B:
+      string binary = decimalToBinary (dec);
+      Console.WriteLine ($"\nBinary equivalent: {binary}");
+      break;
+   case ConsoleKey.H:
+      string hex = decimalToHex (dec);
+      Console.WriteLine ($"\nHexadecimal equivalent: {hex}");
+      break;
+   default:
+      Console.WriteLine ("\nEnter a valid key :(");
+      break;
 }
+string decimalToBinary (int dec) {                                         //This function converts the decimal number to its equivlent binary number
+   string binary = ""; int binRemainder = 0;
+   if (dec == 0)
+      return "0";
+   while (dec > 0) {
+      binRemainder = dec % 2;                                              //The decimal number is first divided by modulus operator
+      binary = binRemainder + binary;                                      //remainder obtained in the previous line is added to the empty binary string
+      dec = dec / 2;                                                       //decimal number is update dby dividing it further by 2
+   }
+   return binary;                                                          //returns the updated binary string as output
+}
+string decimalToHex (int dec) {
+   if (dec == 0)
+      return "0";
+   string hexChars = "0123456789ABCDEF";                                   //hexadecimal characters are defined in a string as hexChars
+   string hex = "";
+   while (dec > 0) {                                                                       
+      hex = hexChars[dec % 16] + hex;                                      //empty string hex is updated by dividing the decimal number by 16 using modulo operator,
+      dec = dec / 16;                                                      //after which the quotient indicates the index of the hexChars string
+   }                                                                       //and is added to the empty string 
+   return hex;
+} 
