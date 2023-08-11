b27c4cd0d2d9d187abbc22c0eb041d7ee85b9a94
Shresh2 <138746515+Shresht07@users.noreply.github.com>
2023-08-11 16:23:51 +0530

    Assignment T05 done and pushed to repo

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,22 @@
-namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+//Prime number checker 
+int number;
+while (true) {
+   Console.Write ("Enter the number: ");                          //Gets an input integer from user
+   if (int.TryParse (Console.ReadLine (), out number))
+      break;
+   else
+      Console.WriteLine ("Enter a valid integer :(");
 }
+bool IsPrime (int number) {                                      //A Boolean function which returns true when the input number is prime
+   if (number <= 1)                                              //If number is less then 1, then it might not be a prime number
+      return false;
+   for (int i = 2; i * i <= number; i++) {
+      if (number % i == 0)                                       //If input is divisible by i, then IsPrime returns it not prime  
+         return false;
+   }
+   return true;
+}
+if (IsPrime (number))
+   Console.WriteLine ($"{number} is a prime number");
+else
+   Console.WriteLine ($"{number} is not a prime number");
