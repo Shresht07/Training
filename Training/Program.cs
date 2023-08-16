30641b4f72da2199c736d41cfe242984c0a616fd
Shresh2 <138746515+Shresht07@users.noreply.github.com>
2023-08-16 14:49:21 +0530

    Assignment T06 done and pushed to repo

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,14 @@
-namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+//Palindrome checker
+Console.Write ("Enter a word: ");                                                            //gets the input from user
+var word = Console.ReadLine ();                                                              
+string WordReverse (string word) {                                                          //This function reverses the words 
+   string wordRev = "";
+   for(int i = word.Length-1; i >=0;i--) {
+      wordRev += word[i];                                                                   //Adds the words in reverse order in the wordRev string
+   }
+   return wordRev;
 }
+if (word == WordReverse (word))                                                             //If the input ford is equal to the reverse of the word
+   Console.WriteLine ($"{WordReverse (word)} is a palindrome of {word}");                   //the it is a palindrome
+else
+   Console.WriteLine ($"{WordReverse (word)} is not a palindrome of {word}");               //Else it is not a palindrome
