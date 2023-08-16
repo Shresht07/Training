0d268c63eaa106c9e8425aeb9c9ce3a194e3980e
Shresh2 <138746515+Shresht07@users.noreply.github.com>
2023-08-16 15:25:13 +0530

    Assignment T08 done and pushed to repo

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,13 @@
-namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+//Multiplication table
+int number, i = 1;
+while (true) {                                                                               //Checks whether the input integer is valid or not
+   Console.Write ("Enter the number: ");
+   if (int.TryParse (Console.ReadLine (), out number) && (number >= 0) || (number <= 0))
+      break;
+   else
+      Console.WriteLine ("Enter a valid integer :(");                                        //If not a valid integer, prompts the user to enter a valid one
 }
+do {
+   Console.WriteLine ($"{number} *{i,3} = {number * i}");                                    //Prints Multiplication table in the specific way
+   i++;                                                                                      //increments by 1
+} while (i <= 10);                                                                           //Checks whether i is lesser than or equal to 10
