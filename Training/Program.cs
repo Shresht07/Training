5f53df3618f483fec13585d3251ef3b2eea5b254
Shresh2 <138746515+Shresht07@users.noreply.github.com>
2023-08-17 10:56:48 +0530

    Assignment T10 done and pushed to repo

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,18 @@
-namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+//Number reverse without string manipulation
+int number;
+while (true) {
+   Console.Write ("Enter the number: ");                                       //Asks user for the number that needs to be reversed
+   if (int.TryParse (Console.ReadLine (), out number) && number >= 0)                  
+      break;
+   else
+      Console.WriteLine ("Enter a valid integer :(");                          //If the input is not a valid integer then prompts the user to enter a valid integer
 }
+int NumberRev (int number) {                                                   //This function reverses a number without string manipulation
+   int result = 0;                                                             //A result variable is created to store the resultant number
+   while (number != 0) {                                                       //If the given number is not equal to 0, the loop enters
+      result = result * 10 + number % 10;                                      //Result is updated 
+      number /= 10;                                                            //Number is also updated
+   }
+   return result;                                                              //Returns the result
+}
+Console.WriteLine ($"Reversed form of given number is {NumberRev (number)}");  //prints the output in reverse form
