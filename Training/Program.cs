dfda45367edbf38802af84de81e1a82cc36a70d4
Shresh2 <138746515+Shresht07@users.noreply.github.com>
2023-08-22 15:22:28 +0530

    Simplification done, logic corrected and pushed to repo

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,22 +1,19 @@
 //Asterisk printed in diamond form
-Console.Write ("Enter max number of stars in the middle row: ");
-int nstars = int.Parse (Console.ReadLine ());
-for (int rows1 = 1; rows1 <= (2 * nstars + 1) / 2; rows1 += 2) {         //Printing a pyramid of stars first based on user's input
-   for (int spaces1 = 1; spaces1 <= nstars - rows1; spaces1++) {         //Spaces are given appropriately from the margin
-      Console.Write (" ");
-   }
-   for (int stars1 = 1; stars1 <= rows1; stars1++) {                     //Stars are printed according to spaces 
-      Console.Write ("* ");
-   }
-   Console.WriteLine ();                                                 //Printing stars in pyramid order
+Console.Write ("Enter number of rows: ");                                           //Mention number of rows to be printed in output 
+int nrows = int.Parse (Console.ReadLine ());                                        //If an odd number is entered, for correct calculation 
+if (nrows % 2 != 0) nrows++;
+for (int rows1 = 1; rows1 <= ((2 * nrows + 1) / 2) - 2; rows1 += 2) {               //The variable rows1 is initiated with 1   
+   SpacesAndStars (nrows, rows1);
 }
-//Printing a pyramind of stars in reverse order right below the above pyramid
-for (int rows2 = ((2 * nstars - 1) / 2) - 2; rows2 >= 1; rows2 -= 2) {   //Printing according to user's input number of stars in the first row       
-   for (int spaces2 = 1; spaces2 <= nstars - rows2; spaces2++) {         //Spacing with n - rows2 number of spaces 
+for (int rows2 = ((2 * nrows + 1) / 2) - 1; rows2 >= 1; rows2 -= 2) {               //The rows2 is intiated with an element < the max no.of stars in first pyramid  
+   SpacesAndStars (nrows, rows2);
+}
+static void SpacesAndStars (int nrows, int rows) {                                  //This method provides the necessary spaces and stars to be printed 
+   for (int spaces2 = 1; spaces2 <= nrows - rows; spaces2++) {                      //Spaces are given as nrows - rows1 or rows2
       Console.Write (" ");
    }
-   for (int stars2 = 1; stars2 <= rows2; stars2++) {                     //Stars are printed according to rows2
+   for (int stars2 = 1; stars2 <= rows; stars2++) {                                 //After entering the necessary spaces, stars are printed 
       Console.Write ("* ");
    }
-   Console.WriteLine ();                                                 //Printing the stars in reverse pyramid order
+   Console.Write ("\n");                                                            //To print asterisk in next line
 }
