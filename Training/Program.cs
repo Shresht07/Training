9d3341ca2d57d7064c13850d14608d261e3aba44
Shresh2 <138746515+Shresht07@users.noreply.github.com>
2023-08-22 10:39:14 +0530

    Ternary conditional operator used, Simplification done and pushed to repo.

--- Training/Program.cs
+++ Training/Program.cs
@@ -14,10 +14,6 @@ List<int> GetNumbers (int nNum) {
    return numbers;
 }
 int GCD (int num1, int num2) {
-   //   if (num2 == 0)
-   //      return num1;
-   //   else
-   //      return GCD (num2, num1 % num2);
    return (num2 == 0 ? num1 : GCD (num2, num1 % num2));
 }
 int GCDforNnum (List<int> numbers) {
