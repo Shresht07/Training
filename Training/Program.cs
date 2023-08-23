58563ea42a800b0b8dec6d5683527607ca328a60
Shresh2 <138746515+Shresht07@users.noreply.github.com>
2023-08-23 09:59:34 +0530

    Digital root calculator

--- Training/Program.cs
+++ Training/Program.cs
@@ -10,11 +10,7 @@ int DigitalRoot (int number) {                                                  
    while (number != 0) {                                                        //Here the number is always not equal to 0, because the digital root of 0 is 0
       result += number % 10;                                                    //The remainder of number mod 10 is added to the result  
       number /= 10;                                                             //The number is then updated
-   }
-   //if (result % 9 == 0)                                                         //
-   //   return 9;                                                                 //
-   //else
-   //   return result % 9;                                                       
+   }                                                
    return (result % 9 == 0 ? 9 : result % 9);
 }
-Console.WriteLine ($"Digital root of {number} is {DigitalRoot (number)}");                 //Prints the digital root of the input
+Console.WriteLine ($"Digital root of {number} is {DigitalRoot (number)}");      //Prints the digital root of the input
