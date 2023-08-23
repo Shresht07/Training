67eb0b968e08f4d20464a2651f5ee2b8bfa2d7ad
Shresht <shresht.b@metamation.com>
2023-08-23 12:12:58 +0530

    Palindrome checker done

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,14 +1,13 @@
 //Palindrome checker
 Console.Write ("Enter a word: ");                                                            //gets the input from user
 var word = Console.ReadLine ();                                                              
-string WordReverse (string word) {                                                          //This function reverses the words 
+static string WordReverse (string word) {                                                          //This function reverses the words 
    string wordRev = "";
    for(int i = word.Length-1; i >=0;i--) {
       wordRev += word[i];                                                                   //Adds the words in reverse order in the wordRev string
    }
    return wordRev;
 }
-if (word == WordReverse (word))                                                             //If the input ford is equal to the reverse of the word
-   Console.WriteLine ($"{WordReverse (word)} is a palindrome of {word}");                   //the it is a palindrome
-else
-   Console.WriteLine ($"{WordReverse (word)} is not a palindrome of {word}");               //Else it is not a palindrome
+//If the input word is equal to the reverse of the word,then it is a palindrome,else it is not a palindrome
+var result = (word == WordReverse (word)) ? ($"{WordReverse (word)} is a palindrome of {word}") : ($"{WordReverse (word)} is not a palindrome of {word}");
+Console.WriteLine (result);
