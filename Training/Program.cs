70850edff2da1685d992568badca984a25856e79
Shresht Baskaran <Shresht.Baskaran@trumpf.com>
2023-08-28 17:15:44 +0530

    Reduced string

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,15 @@
-namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+Console.Write ("Enter a string: ");
+var redstr = Console.ReadLine ();
+Console.WriteLine (ReducedString (redstr));
+static string ReducedString (string redstr) {
+   string str = "";
+   for (int i = 0; i < redstr.Length;) {
+      if (i < redstr.Length - 1 && redstr[i] == redstr[i + 1])
+         i += 2;
+      else {
+         str += redstr[i];
+         i++;
+      }
+   }
+   return str;
 }
