1aac0229902e01e94ae64139142eb8f71aea4bb5
Shresht Baskaran <Shresht.Baskaran@trumpf.com>
2023-08-29 15:02:47 +0530

    Isogram checker assignment done

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,12 @@
-namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+Console.Write ("Enter a string: ");
+string isostr = Console.ReadLine ();
+Console.WriteLine (IsIsogram(isostr)?"Its a isogram":"Not an isogram");
+static bool IsIsogram (string isostr) {
+   char[] result = isostr.ToArray ();
+   Array.Sort(result);
+   for (int i = 0; i < isostr.Length-1;i++) {
+      if (result[i] == result[i + 1])
+         return false;
+   }
+   return true;
 }
