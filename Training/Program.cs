46dc7c1248a8640ff6da7064f57df59a0d7aaa90
Shresht Baskaran <Shresht.Baskaran@trumpf.com>
2023-08-30 12:40:49 +0530

    Password validity checker changes done

--- Training/Program.cs
+++ Training/Program.cs
@@ -5,27 +5,34 @@ Console.WriteLine ("Enter a password which satisfies the following guidelines" +
    "\n3.Password must contain at least one lowercase letter" +
    "\n4.Password must contain atleast one digit" +
    "\n5.Password must contain at least one special character\n");
-while (true) {                                                                            // An infinite loop to ask the user to enter a valid password 
+//for (; ; ) {                                                                                          // An infinite loop to ask the user to enter a valid password 
+//   Console.Write ("Password: ");
+//   string password = Console.ReadLine ();
+//   if (StrongPassword (password) == "Accepted") {
+//      Console.WriteLine (StrongPassword (password));                                                  // If there are no error messages, then accepts the password 
+//      break;
+//   } else
+//      Console.WriteLine (StrongPassword (password));                                                  // Else asks for a valid password
+//}
+while(true) {
    Console.Write ("Password: ");
    string password = Console.ReadLine ();
-   if (StrongPassword (password) == "Strong password") {                                  // If there are no error messages, then accepts the password 
-      Console.WriteLine ("Accepted");
+   while (StrongPassword (password) == "Accepted") { 
+      Console.WriteLine (StrongPassword(password));
       break;
-   } else {
-      Console.WriteLine ($"{StrongPassword (password)}\nInvalid password, Try again.");    // Else asks for a valid password
    }
 }
 static string StrongPassword (string password) {
-   if (password.Length < 6)                                                               // Checks whether the input is less than 6 characters
-      return "Password must contain atleast 6 characters\n";
-   else if (!char.IsUpper (password[0]))                                                  // Checks whether the password starts with an Uppercase letter
-      return "Password should start with an uppercase letter\n";
-   else if (!password.Any (char.IsLower))                                                 // Checks whether the input has any lowercase letter in it
-      return "Password must contain at least one lowercase letter\n";
-   else if (!password.Any (char.IsDigit))                                                 // Checks whether the input has any digits in it
-      return "Password must contain at least one digit\n";
-   else if (!password.Any (char.IsSymbol) && !password.Any (char.IsPunctuation))          // Checks whether the input has a special character in it or not
-      return "Password must contain at least one special character\n";
+   if (password.Length < 6)                                                                           // Checks whether the input is less than 6 characters
+      return "Password must contain atleast 6 characters\nInvalid password, Try again.";
+   else if (!char.IsUpper (password[0]))                                                              // Checks whether the password starts with an Uppercase letter
+      return "Password should start with an uppercase letter\nInvalid password, Try again.";
+   else if (!password.Any (char.IsLower))                                                             // Checks whether the input has any lowercase letter in it
+      return "Password must contain at least one lowercase letter\nInvalid password, Try again.";
+   else if (!password.Any (char.IsDigit))                                                             // Checks whether the input has any digits in it
+      return "Password must contain at least one digit\nInvalid password, Try again.";
+   else if (!password.Any (char.IsSymbol) && !password.Any (char.IsPunctuation))                      // Checks whether the input has a special character in it or not
+      return "Password must contain at least one special character\nInvalid password, Try again.";
    else
-      return "Strong password";                                                           // If all the conditions are met, then the method returns "Strong password"
+      return "Accepted";                                                                              // If all the conditions are met, then the method returns "Strong password"
 }
