10a1687ca63d6edadd48e6f3c26f9d7b64b19be6
Shresh2 <138746515+Shresht07@users.noreply.github.com>
2023-08-30 14:17:22 +0530

    Isogram changes done in output part

--- Training/Program.cs
+++ Training/Program.cs
@@ -3,7 +3,7 @@ Console.Write ("Enter a string: ");                                       // Get
 string isostr = Console.ReadLine ();
 static bool IsIsogram (string isostr) {                                   // A method which checks whether the input is isogram or not
    char[] result = isostr.ToCharArray ();                                 // Spliting the string into single elements 
-   Array.Sort (result);                                                    // Sorting the elements in ascending order 
+   Array.Sort (result);                                                   // Sorting the elements in ascending order 
    for (int i = 0; i < isostr.Length - 1; i++) {
       if (result[i] == result[i + 1])                                     // Checking whether same letter is occured consecutively
          return false;                                                    // Returns false if there same letter is occured consecutively, Since we have arranged it in ascending order
@@ -11,4 +11,4 @@ static bool IsIsogram (string isostr) {                                   // A m
    return true;                                                           // Returns true, if no letter matches
 }
 // If the method returns true, then its an isogram, else not an isogram
-Console.WriteLine (IsIsogram (isostr) ? "Its a isogram" : "Not an isogram");
+Console.WriteLine (IsIsogram (isostr) ? "It's an isogram" : "Not an isogram");
