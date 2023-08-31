6396bf78948054ea3348f68a998ca1c492261b49
Shresh2 <138746515+Shresht07@users.noreply.github.com>
2023-08-31 15:52:19 +0530

    Longest abecedarian word output framed correctly

--- Training/Program.cs
+++ Training/Program.cs
@@ -18,18 +18,16 @@ static string[] GetWordInput (int nWords) {                                     
 string[] inWords = GetWordInput (nWords);                                                      // Call the GetWordInput method to get input words.
 static string AbecedarianWord (string[] inWords) {                                             // A method to find the longest abecedarian word.
    string result = "";                                                                         // Initialize a variable to store the longest abecedarian word found.
-   foreach (string word in inWords) {
-      bool isAbe = true;// Iterate through each word in the input array.
-      for (int i = 0; i < word.Length - 1; i++) {                                                  // Iterate through each letter of each word in the array
-         // Iterating through all the elements and if the adjacent elements are in ascending order and the single word length is larger than the temporary variable length
-         if (word[i] > word[i + 1]) {
-            isAbe = false;
+   foreach (string word in inWords) {                                                          // Iterate through each letter of each word in the array
+      bool isAbe = true;
+      for (int i = 0; i < word.Length - 1; i++) {                                              // Iterate through each letter of each word in the array
+         if (word[i] > word[i + 1]) {                                                          // Checking whether the letters are greater than or not
+            isAbe = false;                                                                     // If greater, then becomes false 
             break;
-         }                                                                  // The temporary variable is updated                                                                                // Else incremented 
+         }
       }
-      if (isAbe && result.Length < word.Length) result = word;
+      if (isAbe && result.Length < word.Length) result = word;                                 // Temporary variable is updated
    }
-   return result;                                                                              // Returns the longest abecedarian word 
+   return (result.Length != 0) ? "Longest abecedarian word is " + result : "No abecedarian word in the given input";
 }
-Console.WriteLine ("Longest abecedarian word is " + AbecedarianWord (inWords));                // Printing the result
-
+Console.WriteLine (AbecedarianWord (inWords));                                                 // Returns the longest abecedarian wordand Printing the result
