7dd2e675e02d506d5d3ab059deaa248f87273c9b
Shresht Baskaran <Shresht.Baskaran@trumpf.com>
2023-09-13 14:46:43 +0530

    T19: Factorial of a number

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,9 +1,9 @@
 Console.Write ("Enter the number to be factorized: ");
-int inputNumber =  int.Parse(Console.ReadLine());
+int inputNumber = int.Parse (Console.ReadLine ());
 static int Factorial (int inputNumber) {
    if (inputNumber == 0)
       return 1;
    else
       return inputNumber * Factorial (inputNumber - 1);
 }
-Console.WriteLine ($"Factorial of the input number is {Factorial(inputNumber)}");
+Console.WriteLine ($"Factorial of the input number is {Factorial (inputNumber)}");
