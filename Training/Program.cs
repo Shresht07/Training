ba37fc5ad0bd7215cf6e002556a1ee5a778d686a
Shresht Baskaran <Shresht.Baskaran@trumpf.com>
2023-09-13 17:23:47 +0530

    Swapping of two variable values

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,10 @@
-namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+static void SwapNum () {
+   Console.Write ("Give a integer value for first variable: ");
+   int a = int.Parse (Console.ReadLine ());
+   Console.Write ("Now for second variable: ");
+   int b = int.Parse (Console.ReadLine ());
+   Console.WriteLine ($"Before swapping a = {a} and b = {b}");
+   (a, b) = (b, a);
+   Console.WriteLine ($"After swapping a = {a} and b = {b}");
 }
+SwapNum ();
