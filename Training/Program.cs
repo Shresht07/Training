face78694471cc4a98657f1470826e68d2f61423
Shresht Baskaran <Shresht.Baskaran@trumpf.com>
2023-09-13 14:42:17 +0530

    Factorial of a given number completed

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,9 @@
-namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+Console.Write ("Enter the number to be factorized: ");
+int inputNumber =  int.Parse(Console.ReadLine());
+static int Factorial (int inputNumber) {
+   if (inputNumber == 0)
+      return 1;
+   else
+      return inputNumber * Factorial (inputNumber - 1);
 }
+Console.WriteLine ($"Factorial of the input number is {Factorial(inputNumber)}");
