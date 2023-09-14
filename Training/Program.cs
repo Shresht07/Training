4e3de8fbd5317b9fc97f842a22361eb44f2edfd8
Shresht Baskaran <Shresht.Baskaran@trumpf.com>
2023-09-14 15:34:15 +0530

    Nth Armstrong number

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,28 @@
-namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+int nArmsNum;
+for (; ; ) {
+   Console.Write ("Mention the Nth number of armstrong number to be displayed within 25: ");
+   if (int.TryParse (Console.ReadLine (), out nArmsNum) && nArmsNum <= 25)
+      break;
+   else if (nArmsNum > 25)
+      Console.WriteLine ("Enter within the given range");
+   else
+      Console.WriteLine ("Enter a valid integer");
+}
+static long NthArmstrongNumber (int nArmsNum) {
+   long[] result = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 371, 407, 1634, 8208, 9474, 54748, 92727, 93084, 548834, 1741725, 4210818, 9800817, 9926315, 24678051 };
+   return result[nArmsNum - 1];
+}
+switch (nArmsNum) {
+   case 1:
+      Console.Write ($"The {nArmsNum}st armstrong number is {NthArmstrongNumber (nArmsNum)}");
+      break;
+   case 2:
+      Console.Write ($"The {nArmsNum}nd armstrong number is {NthArmstrongNumber (nArmsNum)}");
+      break;
+   case 3:
+      Console.Write ($"The {nArmsNum}rd armstrong number is {NthArmstrongNumber (nArmsNum)}");
+      break;
+   default:
+      Console.WriteLine ($"The {nArmsNum}th armstrong number is {NthArmstrongNumber (nArmsNum)}");
+      break;
 }
