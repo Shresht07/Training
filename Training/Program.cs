de9edc9b87418b1fcd0151545c95b3a71f19eac1
Shresht Baskaran <Shresht.Baskaran@trumpf.com>
2023-09-15 14:55:51 +0530

    space added between function and main line

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,6 +1,7 @@
 Console.Write ("Enter the number to be factorized: ");
 int inputNumber = int.Parse (Console.ReadLine ());
 Console.WriteLine ($"Factorial of the input number is {Factorial (inputNumber)}");
+
 static long Factorial (int inputNumber) {
    if (inputNumber == 0)
       return 1;
