fe2d6c3593c565503d636fba9641e0c34ecf8b56
Shresht Baskaran <Shresht.Baskaran@trumpf.com>
2023-09-15 11:46:38 +0530

    Datatype changed and function placed after main statement

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,9 +1,12 @@
+using System.Numerics;
+
 Console.Write ("Enter the number to be factorized: ");
 int inputNumber = int.Parse (Console.ReadLine ());
-static int Factorial (int inputNumber) {
+Console.WriteLine ($"Factorial of the input number is {Factorial (inputNumber)}");
+static BigInteger Factorial (int inputNumber) {
+   BigInteger result = inputNumber * Factorial (inputNumber - 1);
    if (inputNumber == 0)
       return 1;
    else
-      return inputNumber * Factorial (inputNumber - 1);
+      return result;
 }
-Console.WriteLine ($"Factorial of the input number is {Factorial (inputNumber)}");
