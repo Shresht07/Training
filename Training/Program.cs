5c0ac1f8d5720ae67299cfbe59be12152a4c015c
Shresht Baskaran <Shresht.Baskaran@trumpf.com>
2023-09-15 11:48:20 +0530

    changes done

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,12 +1,9 @@
-using System.Numerics;
-
 Console.Write ("Enter the number to be factorized: ");
 int inputNumber = int.Parse (Console.ReadLine ());
 Console.WriteLine ($"Factorial of the input number is {Factorial (inputNumber)}");
-static BigInteger Factorial (int inputNumber) {
-   BigInteger result = inputNumber * Factorial (inputNumber - 1);
+static long Factorial (int inputNumber) {
    if (inputNumber == 0)
       return 1;
    else
-      return result;
+      return inputNumber * Factorial (inputNumber - 1);
 }
