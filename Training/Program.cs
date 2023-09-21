65bfcf4d7b031aba843f60522e0ec47ac7b424ae
Shresht Baskaran <Shresht.Baskaran@trumpf.com>
2023-09-21 15:06:30 +0530

    Integral part and fractional part display

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,26 +1,61 @@
-decimal inputNum;
-for (; ; ) {
-   Console.WriteLine ("Enter a decimal number: ");
-   if (decimal.TryParse (Console.ReadLine (), out inputNum) && inputNum > 0)
-      break;
-   else
-      Console.WriteLine ("Enter a valid decimal number");
-}
-Console.WriteLine ($"Integral Part: {IntegralPart (inputNum)}\nFractional Part: {Fractionalpart (inputNum)}");
-static int IntegralPart (decimal inputNum) {
-   int integralRes = 0;
-   integralRes += (int)Math.Floor (inputNum);
-   if (integralRes == 0)
-      return 0;
-   else 
-      return integralRes;
-}
-static string Fractionalpart(decimal inputNum) {
-   decimal fractionalRes = 0;
-   fractionalRes += inputNum - IntegralPart(inputNum);
-   if (fractionalRes == 0)
-      return "No Fractional Part";
-   else
-      return fractionalRes.ToString ().Remove (0, 2).Insert(1," ");
-}
+internal class Program {
+   /// <summary>
+   /// 
+   /// </summary>
+   /// <param name="args"></param>
+   /// <returns>
+   private static void Main (string[] args) {
+      decimal inputNum;
+      for (; ; ) {
+         Console.Write ("Enter a decimal number: ");
+         if (decimal.TryParse (Console.ReadLine (), out inputNum) && inputNum > 0)
+            break;
+         else
+            Console.WriteLine ("Enter a valid decimal number");
+      }
+      string integralValues = IntegralPart (inputNum);
+      string fractionalValues = FractionalPart (inputNum);
+      IntegralPartWithSpaces (integralValues);
+      FractionalPartWithSpaces (fractionalValues);
 
+      /// <summary> Return the integral part of a input decimal number.</summary>
+      /// <param name="inputNum"></param>
+      static string IntegralPart (decimal inputNum) {
+         int integralRes = 0;
+         integralRes += (int)Math.Floor (inputNum);
+         if (integralRes == 0)
+            return "No Integral part";
+         else
+            return integralRes.ToString ();
+      }
+
+      /// <summary> Returns the fractional part of the input decimal number</summary>
+      /// <param name="inputNum"></param>
+      static string FractionalPart (decimal inputNum) {
+         decimal fractionalRes = 0;
+         fractionalRes += inputNum - Math.Floor (inputNum);
+         if (fractionalRes == 0)
+            return "No Fractional Part";
+         else
+            return fractionalRes.ToString ().Remove (0, 2);
+      }
+
+      /// <summary> Prints the Integral part of the input number with spaces </summary>
+      /// <param name="intet"></param>
+      static void IntegralPartWithSpaces (string integralValues) {
+         char[] intPart = integralValues.ToCharArray ();
+         Console.Write ("Integral Part: ");
+         for (int i = 0; i < intPart.Length; i++)
+            Console.Write (intPart[i] + " ");
+      }
+
+      /// <summary> Prints the Integral part of the input number with spaces </summary>
+      /// <param name="args"></param>
+      static void FractionalPartWithSpaces (string fractionalValues) {
+         char[] fracPart = fractionalValues.ToCharArray ();
+         Console.Write ("\nFractional Part: ");
+         for (int i = 0; i < fracPart.Length; i++)
+            Console.Write (fracPart[i] + " ");
+      }
+   }
+}
