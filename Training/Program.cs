b47f562accdbe929f1676fd7a0a8aa9727690f8a
Shresht Baskaran <Shresht.Baskaran@trumpf.com>
2023-09-21 15:23:02 +0530

    swaping of variables

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,17 @@
-namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+// Swaping of variables 
+Console.Write ("Give a integer value for first variable: ");
+int a = int.Parse (Console.ReadLine ());
+Console.Write ("Now for second variable: ");
+int b = int.Parse (Console.ReadLine ());
+Console.WriteLine ($"Before swapping a = {a} and b = {b}");
+SwapNum (ref a, ref b);
+Console.WriteLine ($"After swaping a = {a} and b = {b}");
+
+/// <summary> Swaps the variable a and b to b and a respectively</summary>
+/// <int name="a",<int name = "b">></void>
+static void SwapNum (ref int a, ref int b) {
+   int temp = 0;
+   a = temp;
+   temp = b;
+   b = temp;
 }
