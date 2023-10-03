b429afd127292ec9f4f38187057e3d9e9ef5215e
Shresht Baskaran <Shresht.Baskaran@trumpf.com>
2023-10-03 14:09:56 +0530

    Assignment T24: Chocolate Wrappers - done

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,17 @@
-namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+Console.Write ("Mention the amount money you have: ");
+int amount = int.Parse (Console.ReadLine ());
+Console.Write ("Enter the price of a chocolate: ");
+int price = int.Parse (Console.ReadLine ());
+Console.Write ("Mention number of wrappers can be exchanged in order to get one chocolate: ");
+int wrappers = int.Parse (Console.ReadLine ());
+(int, int, int) result = ChocolateWrappers (amount, price, wrappers);
+Console.Write ($"Maxmimum number of chocolates bought(C): {result.Item2}\nBalance amount(X): {result.Item1}\nBalance no.of wrappers(W): {result.Item3}");
+static (int, int, int) ChocolateWrappers (int amount, int price, int wrappers) {
+   int maxChocolates = amount / price, balAmount = amount % price, balWrappers = maxChocolates;
+   do {
+      balWrappers -= wrappers;
+      balWrappers++;
+      maxChocolates++;
+   } while (balWrappers >= wrappers);
+   return (balAmount, maxChocolates, balWrappers);
 }
