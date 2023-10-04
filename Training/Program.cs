8c7fc6b6e7f4518c794cb381ee6c553df51bc777
Shresh2 <138746515+Shresht07@users.noreply.github.com>
2023-10-04 11:26:19 +0530

    T25: Swap and sort special characters done

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,43 @@
-namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+string inputStr, inSplChar;
+for (; ; ) {
+   Console.Write ("Enter a string: ");
+   inputStr = Console.ReadLine ();
+   if (inputStr == "")
+      Console.WriteLine ("Enter a valid string. Empty string is not allowed");
+   else
+      break;
 }
+for (; ; ) {
+   Console.Write ("Mention the special character to be swapped: ");
+   inSplChar = Console.ReadLine ();
+   if (inSplChar == "")
+      Console.WriteLine ("Enter a valid special character. Entering empty special character is not allowed");
+   else
+      break;
+}
+Console.Write ("Mention the order: ");
+string order = Console.ReadLine ();
+SortAndSwapSplChars (inputStr, inSplChar, order);
+
+static void SortAndSwapSplChars (string inputStr, string inSplChar, string order = "") {
+   char splChar = char.Parse (inSplChar);
+   char[] inputCharArray = inputStr.ToCharArray ();
+   List<char> tempList = inputCharArray.ToList ();
+   if (inputStr.Contains (splChar)) {
+      tempList.RemoveAll (ch => ch == splChar);
+      tempList.Sort ();
+      tempList.ToArray ();
+   } else
+      Array.Sort (inputCharArray);
+   if (order == "descending") {
+      tempList.Sort ();
+      tempList.Reverse ();
+   }
+   int count = inputStr.Count (c => c == splChar);
+   for (int i = 0; i < count; i++)
+      tempList.Add (splChar);
+   for (int i = 0; i < tempList.Count - 1; i++)
+      Console.Write (tempList[i] + ",");
+   Console.Write (tempList.Last ());
+}
+ 
