0e9f69fe356acb00f0edbc944674ab5d1e66a39b
Shresht Baskaran <Shresht.Baskaran@trumpf.com>
2023-10-05 14:48:10 +0530

    T24 - changes done

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,12 +1,9 @@
-Console.Write ("Mention the amount money you have: ");
-int amount = int.Parse (Console.ReadLine ());
-Console.Write ("Enter the price of a chocolate: ");
-int price = int.Parse (Console.ReadLine ());
-Console.Write ("Mention number of wrappers can be exchanged in order to get one chocolate: ");
-int wrappers = int.Parse (Console.ReadLine ());
-(int, int, int) result = ChocolateWrappers (amount, price, wrappers);
-Console.Write ($"Maxmimum number of chocolates bought(C): {result.Item2}\nBalance amount(X): {result.Item1}\nBalance no.of wrappers(W): {result.Item3}");
-static (int, int, int) ChocolateWrappers (int amount, int price, int wrappers) {
+int amount = InputValidityChecker ("Mention the amount of money you have: ", "Enter amount as an integer value");
+int price = InputValidityChecker("Enter the price of a chocolate: ", "Enter the price as an integer value");
+int wrappers = InputValidityChecker("Mention number of wrappers can be exchanged in order to get one chocolate: ", "Enter a valid integer as an input");
+(int x, int c, int w) = ChocolateWrappers (amount, price, wrappers);
+Console.Write ($"Maxmimum number of chocolates bought(C): {c}\nBalance amount(X): {x}\nBalance no.of wrappers(W): {w}");
+static (int c, int x, int w) ChocolateWrappers (int amount, int price, int wrappers) {
    int maxChocolates = amount / price, balAmount = amount % price, balWrappers = maxChocolates;
    while (balWrappers >= wrappers) {
       balWrappers -= wrappers;
@@ -14,4 +11,16 @@ static (int, int, int) ChocolateWrappers (int amount, int price, int wrappers) {
       maxChocolates++;
    }
    return (balAmount, maxChocolates, balWrappers);
+}
+
+static int InputValidityChecker (string questions,string warningMsg) {
+   int input;
+   for (; ; ) {
+      Console.Write (questions);
+      if (int.TryParse (Console.ReadLine (), out input) && input >= 0)
+         break;
+      else
+         Console.WriteLine (warningMsg);
+   }
+   return input;
 }
