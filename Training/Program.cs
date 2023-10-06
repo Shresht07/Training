06122d68382a7f2ca4c3b98ed54582fd08c5f07b
Shresh2 <138746515+Shresht07@users.noreply.github.com>
2023-10-06 15:46:16 +0530

    changes done

--- Training/Program.cs
+++ Training/Program.cs
@@ -10,10 +10,9 @@ for (; ; ) {
    }
 }
 List<string> combinations = GeneratePermutations (inWord);
-
-foreach (string permutation in combinations) {
+foreach (string permutation in combinations) 
    Console.WriteLine (permutation);
-}
+
 static List<string> GeneratePermutations (string inWord) {
    List<string> result = new ();
    if (inWord.Length == 1) {
@@ -21,14 +20,11 @@ static List<string> GeneratePermutations (string inWord) {
       return result;
    }
    for (int i = 0; i < inWord.Length; i++) {
-      string currentWord = inWord[i].ToString();
+      string currentWord = inWord[i].ToString ();
       string remainingWord = inWord.Remove (i, 1);
-
       List<string> remainingPermutations = GeneratePermutations (remainingWord);
-
-      foreach (string permutation in remainingPermutations) {
+      foreach (string permutation in remainingPermutations)
          result.Add (currentWord + permutation);
-      }
    }
    return result;
 }
