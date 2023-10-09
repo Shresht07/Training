d49b5e5c831df4af84a2dfba079527e5fb56df53
Shresh2 <138746515+Shresht07@users.noreply.github.com>
2023-10-09 12:59:19 +0530

    Final changes done

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,51 +1,56 @@
-int nArmsNum;
-for (; ; ) {
-   Console.Write ("Mention the Nth number of armstrong number to be displayed within 25: ");
-   if (int.TryParse (Console.ReadLine (), out nArmsNum) && nArmsNum <= 25)
-      break;
-   else if (nArmsNum > 25)
-      Console.WriteLine ("Enter within the given range");
-   else
-      Console.WriteLine ("Enter a valid integer");
-}
-int nthArmsNum = CalculateNthArmstrong (nArmsNum);
-switch (nArmsNum) {
-   case 1:
-      Console.Write ($"The {nArmsNum}st armstrong number is {nthArmsNum}");
-      break;
-   case 2:
-      Console.Write ($"The {nArmsNum}nd armstrong number is {nthArmsNum}");
-      break;
-   case 3:
-      Console.Write ($"The {nArmsNum}rd armstrong number is {nthArmsNum}");
-      break;
-   default:
-      Console.Write ($"The {nArmsNum}th armstrong number is {nthArmsNum}");
-      break;
-}
-
-static int ArmstrongNumber (int inputNumber) {
-   List<int> digits = new ();
-   int result = 0;
-   if (inputNumber == 0)
-      return 0;
-   while (inputNumber > 0) {
-      digits.Add (inputNumber % 10);
-      inputNumber /= 10;
+// ---------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch- July 2023
+// Copyright (c) Metamation India.                                               
+// ---------------------------------------------------------------------------      
+// Program.cs                                                                    
+// <TODO: Write a function to determine the nth Armstrong number input index that needs to be passed as a command-line argument to the application.
+// For example, Armstrong.exe 12 should print as 371. Maximum input can be restricted to 25>
+// ---------------------------------------------------------------------------------------
+namespace Training {
+   #region TestProgram --------------------------------------------------------------------------
+   /// <summary>Finding the Nth Armstrong number</summary>
+   internal class Program {
+      #region Methods ---------------------------------------------
+      private static void Main (string[] args) {
+         for (; ; ) {
+            Console.Write ("Mention the Nth number of armstrong number to be displayed within 25: ");
+            if (int.TryParse (Console.ReadLine (), out int mArmsNum) && mArmsNum <= 25) {
+               int nthArmsNum = CalculateNthArmstrong (mArmsNum);
+               Console.WriteLine (nthArmsNum);
+               break;
+            } else if (mArmsNum > 25)
+               Console.WriteLine ("Enter within the given range");
+            else
+               Console.WriteLine ("Enter a valid integer");
+         }
+         /// <summary>Takes input of the user and returns true if input is armstrong number</summary>
+         /// <param name="inputNumber">User input</param>
+         /// <returns>If the input is armstrong number or not</returns>
+         static bool IsArmstrongNumber (int inputNumber) {
+            int temp = inputNumber;
+            int result = 0;
+            while (temp > 0) {
+               int remainder = temp % 10;
+               result += (int)Math.Pow (remainder, inputNumber.ToString ().Length);
+               temp /= 10;
+            }
+            return result == inputNumber;
+         }
+         /// <summary>Calculates the Nth Armstrong number</summary>
+         /// <param name="armsNum">User input</param>
+         /// <returns>Nth armstrong number</returns>
+         static int CalculateNthArmstrong (int armsNum) {
+            if (armsNum == 0) return 0;
+            int count = 1, num = 1;
+            while (count < armsNum) {
+               num++;
+               if (IsArmstrongNumber (num))
+                  count++;
+            }
+            return num;
+         }
+      }
+      #endregion
    }
-   foreach (int digit in digits) {
-      result += (int)Math.Pow (digit, digits.Count);
-   }
-   return result;
-}
-
-static int CalculateNthArmstrong (int nArmsNum) {
-   if (nArmsNum == 1) return 0;
-   int count = 1, num = 1;
-   while (count < nArmsNum) {
-      num++;
-      if (ArmstrongNumber (num) == num)
-         count++;
-   }
-   return num;
+   #endregion
 }
