d5e58b20ceeff070f21b1fab3c39072ddf9bd07a
Shresh2 <138746515+Shresht07@users.noreply.github.com>
2023-10-10 12:56:17 +0530

    T25: Sort and swap special character - changes done

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,33 +1,62 @@
-string inputStr = InputValidityChecker ("Enter a string: ", "Enter a valid string. Empty string is not allowed");
-string inSplChar = InputValidityChecker("Mention the special character to be swapped: ", "Enter a valid special character. Entering empty special character is not allowed");
-Console.Write ("Mention the order: ");
-string order = Console.ReadLine ();
-SortAndSwapSplChars (inputStr, inSplChar, order);
+// ---------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch- July 2023
+// Copyright (c) Metamation India.                                               
+// ---------------------------------------------------------------------------      
+// Program.cs                                                                    
+// <TODO:Given a character array A, along with special character S and sort order O (default order is Ascending)
+// print the sorted array by keeping the elements matching S to the last of the array>
+// ---------------------------------------------------------------------------------------
+namespace Training {
+   #region TestProgram --------------------------------------------------------------------------
+   /// <summary>Sort and swap special character</summary>
+   internal class Program {
+      #region Methods ---------------------------------------------
+      /// <summary>This method prints swaped characters</summary>
+      /// <param name="args">arguments</param>
+      private static void Main (string[] args) {
+         string inputStr = InputValidityChecker ("Enter a string: ", "Enter a valid string. Empty string is not allowed");
+         string inSplChar = InputValidityChecker ("Mention the special character to be swapped: ", "Enter a valid special character. Entering empty special character is not allowed");
+         Console.Write ("Mention the order: ");
+         string order = Console.ReadLine ();
+         SortAndSwapSplChars (inputStr, inSplChar, order);
 
-static void SortAndSwapSplChars (string inputStr, string inSplChar, string order) {
-   char splChar = char.Parse (inSplChar);
-   List<char> tempList = inputStr.ToList ();
-   if (inputStr.Contains (splChar)) {
-      tempList.RemoveAll (ch => ch == splChar);
-      tempList.Sort ();
-      tempList.ToArray ();
-   } else tempList.Sort ();
-   tempList = order == "descending" ? tempList.OrderDescending ().ToList () : tempList.Order ().ToList ();
-   int count = inputStr.Count (c => c == splChar);
-   for (int i = 0; i < count; i++)
-      tempList.Add (splChar);
-   for (int i = 0; i < tempList.Count - 1; i++)
-      Console.Write (tempList[i] + ",");
-   Console.Write (tempList.Last ());
-}
+         /// <summary>This method sorts and swaps according to the input special character</summary>
+         /// <param name="inputStr">User input</param>
+         /// <param name="inSplChar">User input for special character</param>
+         /// <param name="order">arrangement order</param>
+         /// <returns>input integer</returns>
+         static void SortAndSwapSplChars (string inputStr, string inSplChar, string order) {
+            char splChar = char.Parse (inSplChar);
+            List<char> tempList = inputStr.ToList ();
+            if (inputStr.Contains (splChar)) {
+               tempList.RemoveAll (ch => ch == splChar);
+               tempList.ToArray ();
+            }
+            tempList.Sort ();
+            tempList = order == "descending" ? tempList.OrderDescending ().ToList () : tempList.Order ().ToList ();
+            int count = inputStr.Count (c => c == splChar);
+            for (int i = 0; i < count; i++)
+               tempList.Add (splChar);
+            Console.Write (string.Join (",", tempList));
+         }
 
-static string InputValidityChecker (string questions, string warningMsg) {
-   string input;
-   for (; ; ) {
-      Console.Write (questions);
-      input = Console.ReadLine ();
-      if (input != "") break;
-      else Console.WriteLine (warningMsg);
+         /// <summary>Checks whether the input is valid or not</summary>
+         /// <param name="input">User input</param>
+         /// <returns>input integer</returns>
+         static string InputValidityChecker (string questions, string warningMsg) {
+            string input;
+            for (; ; ) {
+               Console.Write (questions);
+               input = Console.ReadLine ();
+               if (input != "")
+                  break;
+               else
+                  Console.WriteLine (warningMsg);
+            }
+            return input;
+         }
+      }
+      #endregion
    }
-   return input;
+   #endregion
 }
