1abf6bdb6ccf520a03732b9e409bf58bd2ec94b8
Shresht Baskaran <Shresht.Baskaran@trumpf.com>
2023-10-11 14:39:50 +0530

    Changes done

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,8 +1,8 @@
 // ---------------------------------------------------------------------------------------
 // Training ~ A training program for new joinees at Metamation, Batch- July 2023
-// Copyright (c) Metamation India.                                               
-// ---------------------------------------------------------------------------      
-// Program.cs                                                                    
+// Copyright (c) Metamation India.
+// ---------------------------------------------------------------------------
+// Program.cs
 // <TODO: //Write a program to help solve a New-York Times style Spelling Bee.
 // The program can assume a word list is given as a text file, and that the daily choice of 7 letters is provided as an array of 7 chars>
 // ---------------------------------------------------------------------------------------
@@ -16,52 +16,39 @@ namespace Training {
       private static void Main (string[] args) {
          string[] wordList = File.ReadAllLines (@"C:\Users\baskaransh\Downloads\words.txt");
          char[] letters = { 'U', 'X', 'A', 'L', 'T', 'N', 'E' };
-         Dictionary<string, int> validWords = FindValidWords (wordList, letters);
-         var res = validWords.OrderByDescending (pair => pair.Value).ThenBy (pair => pair.Key).ToList ();
-         Console.WriteLine ("Word\t\tScore");
+         Dictionary<string, int> validWords = GetValidWords (wordList, letters);
+         var result = validWords.OrderByDescending (pair => pair.Value).ThenBy (pair => pair.Key).ToList ();
          int total = validWords.Sum (x => x.Value);
-         foreach (var result in res) 
-            if (IsPangram (result.Key, letters)) {
+         foreach (var res in result)
+            if (IsPangram (res.Key, letters)) {
                Console.ForegroundColor = ConsoleColor.Green;
-               Console.WriteLine ($"{result.Key}\t{result.Value}");
+               Console.WriteLine ($"{res.Value}. {res.Key}");
                Console.ResetColor ();
-            } 
-         else if (result.Value == 9) Console.WriteLine ($"{result.Key}\t{result.Value}");
-            else Console.WriteLine ($"{result.Key}\t\t{result.Value}");
-         Console.WriteLine ($"Total\t\t{total}");
-         /// <summary>This method checks if the iterated word is valid or not</summary>
-         /// <param name="word">word from Word list</param>
-         /// <param name="letters">Characters list</param>
-         /// <returns>Word is valid or not</returns>
-         static bool IsWordValid (string word, char[] letters) {
-            foreach (char letter in word)
-               if (!letters.Contains (letter)) return false;
-            return true;
-         }
-         /// <summary>This method checks if the iterated word is valid or not</summary>
-         /// <param name="wordList">Word list</param>
-         /// <param name="letters">Characters list</param>
-         /// <returns>Valid words addition to a resultant dictionary</returns>
-         static Dictionary<string, int> FindValidWords (string[] wordList, char[] letters) {
-            var validWords = new Dictionary<string, int> ();
-            foreach (string word in wordList) 
-               if (word.Length >= 4 && word.Contains (letters[0]) && IsWordValid (word, letters)) {
-                  int score = IsPangram (word, letters) ? word.Length + 7 : word.Length;
-                  validWords[word] = score;
                }
-            return validWords;
-         }
-         /// <summary>This method checks if the iterated word is pangram or not</summary>
-         /// <param name="word">word from Word list</param>
-         /// <param name="letters">Characters list</param>
-         /// <returns>Word is pangram or not</returns>
-         static bool IsPangram (string word, char[] letters) {
-            foreach (char letter in letters)
-               if (!word.Contains (letter)) return false;
-            return true;
-         }
+            else Console.WriteLine ($" {res.Value}. {res.Key}");
+         Console.WriteLine ($"----\n{total} total");
       }
-      #endregion
+
+      /// <summary>This method provides a dictionary of words which are valid</summary>
+      /// <param name="wordList">Word list</param>
+      /// <param name="letters">Characters list</param>
+      /// <returns>Valid words addition to a resultant dictionary</returns>
+      static Dictionary<string, int> GetValidWords (string[] wordList, char[] letters) {
+         var validWords = new Dictionary<string, int> ();
+         foreach (string word in wordList)
+            if (word.Length >= 4 && word.Contains (letters[0]) && word.All (letters.Contains)) {
+               int score = IsPangram (word, letters) ? word.Length + 7 : word.Length <= 4 ? 1 : word.Length;
+               validWords[word] = score;
+            }
+         return validWords;
+      }
+
+      /// <summary>This method checks if the iterated word is pangram or not</summary>
+      /// <param name="word">word from Word list</param>
+      /// <param name="letters">Characters list</param>
+      /// <returns>Word is pangram or not</returns>
+      static bool IsPangram (string word, char[] letters) => letters.All (word.Contains);
    }
+      #endregion
    #endregion
 }
