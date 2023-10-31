984d549eac338c6edfb95bdadfcd38290e5a84db
Shresht Baskaran <Shresht.Baskaran@trumpf.com>
2023-10-31 12:42:51 +0530

    Assignment T27 done

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,7 +1,38 @@
 namespace Training {
-    internal class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello, World!");
-        }
-    }
+   class TStack<T> {
+      T[] mVariables;
+      public TStack () => mVariables = new T[4];
+      public int Count => mSize;
+      public int Capacity => mVariables.Length;
+      public void Push (T element) {
+         if (Count == Capacity) Array.Resize (ref mVariables, Capacity * 2);
+         mVariables[mSize++] = element;
+      }
+
+      public T Pop () {
+         try {
+            --mSize;
+            return mVariables[mSize];
+         } catch {
+            throw new InvalidOperationException ("Could not pop due to empty stack");
+         }
+      }
+
+      public T Peek () {
+         try {
+            return mVariables[mSize - 1];
+         }
+         catch {
+            throw new InvalidOperationException ("Could not peek due to empty stack");
+         }
+      }
+
+      public bool IsEmpty { get { return mVariables.Length == 0; } }
+      int mSize = 0;
+   }
+   internal class Program {
+      static void Main (string[] args) {
+         TStack<string> stack = new ();
+      }
+   }
 }
