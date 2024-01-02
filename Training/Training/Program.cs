79bb5b84f2a5c519025525c49448553ca73438df
Shresht Baskaran <Shresht.Baskaran@trumpf.com>
2024-01-02 10:03:54 +0530

    Adding all files before implementing

--- Training/Training/Program.cs
+++ Training/Training/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿namespace Training;
+class Program {
+   static void Main (string[] args) {
+      var eval = new Evaluator ();
+      for (; ; ) {
+         Console.Write ("> ");
+         string text = Console.ReadLine () ?? "";
+         if (text == "exit") break;
+         try {
+            double result = eval.Evaluate (text);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine (result);
+         } catch (Exception e) {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine (e.Message);
+         }
+         Console.ResetColor ();
+      }
+   }
+}
